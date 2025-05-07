88a293d296b398c3d46a5b0d4631317b46fdf2ca
piexlMax(奇淼 <qimiaojiangjizhao@gmail.com>
2025-05-07 17:06:47 +0800

    chore: 删除多余的文件和资源
    清理项目中未使用的图片、文档、组件和配置文件，以减少项目体积并提高维护性。

--- uni-cli/src/uni_modules/wu-safe-bottom/components/wu-safe-bottom/props.js
+++
@@ -1,5 +0,0 @@
-export default {
-    props: {
-		...uni.$w?.props?.safeBottom
-    }
-}
--- uni/api/product.js
+++
@@ -1,9 +0,0 @@
-import {request} from '@/utils/request.js'
-
-// 获取单个商品详情
-export const findGood = (params) => {
-    return request({
-		url:`/good/findGood?ID=${params}`,
-        method: 'get',
-    })
-}
