d4da5e634437f18533d9c3b3e24a1c720b43211c
pixelmaxQM <flipped_aurora@163.com>
2025-05-18 18:40:07 +0800

    fix:修正代码，让代码可以运行

--- server/api/v1/client/user.go
+++ server/api/v1/client/user.go
@@ -328,7 +328,7 @@ func (clientUserApi *ClientUserApi) TokenNext(c *gin.Context, user client.Client
 	}
 
 	if jwtStr, err := jwtService.GetRedisJWT(user.Username); errors.Is(err, redis.Nil) {
-		if err := jwtService.SetRedisJWT(token, user.Username); err != nil {
+		if err := utils.SetRedisJWT(token, user.Username); err != nil {
 			global.GVA_LOG.Error("设置登录状态失败!", zap.Error(err))
 			response.FailWithMessage("设置登录状态失败", c)
 			return
@@ -349,7 +349,7 @@ func (clientUserApi *ClientUserApi) TokenNext(c *gin.Context, user client.Client
 			response.FailWithMessage("jwt作废失败", c)
 			return
 		}
-		if err := jwtService.SetRedisJWT(token, user.Username); err != nil {
+		if err := utils.SetRedisJWT(token, user.Username); err != nil {
 			response.FailWithMessage("设置登录状态失败", c)
 			return
 		}
