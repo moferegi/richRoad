df919353f2ab56d4a983d197769e63cb83bb82a2
mofer <mofer520egi@gmail.com>
2026-07-09 12:24:36 +0800

    修复线上配置文件（配合上个版本，除了myql数据库线上配置，其它的都在本地配置）

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -14,9 +14,9 @@ func Cors() gin.HandlerFunc {
 		method := c.Request.Method
 		origin := c.Request.Header.Get("Origin")
 		c.Header("Access-Control-Allow-Origin", origin)
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,X-User-Id,Accept-Language")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,X-User-Id,Accept-Language,X-Client-Platform,X-Resp-Encrypt,X-Req-Ts,X-Req-Nonce,X-Req-Sign")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, New-Token, New-Expires-At")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, New-Token, New-Expires-At, X-Resp-Encrypted")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
 		// 放行所有OPTIONS方法
